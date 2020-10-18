25b49cf07504940019111b5aaa97fe9210992a5c
Vrishab M Sharma <vrishabmsharma@gmail.com>
2020-10-18 21:28:53 +0530

    Updates

---
+++ precache-manifest.79a304ba554086c8d25824e3de0f0580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824094f334f39a51f72b26c584208815",
+    "url": "/VrishabM/react-demo-app/index.html"
+  },
+  {
+    "revision": "92b2f4a9c3bdace48c4c",
+    "url": "/VrishabM/react-demo-app/static/css/main.e8d76486.chunk.css"
+  },
+  {
+    "revision": "3d3c6fd4985ba479c956",
+    "url": "/VrishabM/react-demo-app/static/js/2.f59f507d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/VrishabM/react-demo-app/static/js/2.f59f507d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b2f4a9c3bdace48c4c",
+    "url": "/VrishabM/react-demo-app/static/js/main.61f5bcf1.chunk.js"
+  },
+  {
+    "revision": "41ecb118f9b940f72718",
+    "url": "/VrishabM/react-demo-app/static/js/runtime-main.ab0e3259.js"
+  }
+]);
